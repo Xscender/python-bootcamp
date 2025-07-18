cb34175ab59a668cd24bbb53fa231e9bba8919d4
Kiyun Kim <kiyn2403@gmail.com>
2025-07-18 12:35:42 +0900

    Update day-08/project/main.py

--- beginner/day-08/project/main.py
+++ beginner/day-08/project/main.py
@@ -1,40 +1,34 @@
-# TODO-1: Import and print the logo from art.py when the program starts.
 import art
 print(art.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(original_text, shift_amount, encode_or_decode):
-    output_text = ""
-
+    output_text = "" # Initializes an empty string to store encrypted or decrypted results.
     for letter in original_text:
-
-        # TODO-2: What happens if the user enters a number/symbol/space?
         if letter not in alphabet:
             output_text += letter
         else:
             if encode_or_decode == "decode":
                 shift_amount *= -1
-
             shifted_position = alphabet.index(letter) + shift_amount
-            shifted_position %= len(alphabet)
+            shifted_position %= len(alphabet) # When the shifted position is out of alphabetical range (e.g. when the +3 shift from 'z' should be 'c')
             output_text += alphabet[shifted_position]
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
-
-# TODO-3: Can you figure out a way to restart the cipher program?
-
+# End or restart
 should_continue = True
 
 while should_continue:
-
+    
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
-
+    
     caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
-
+    
     restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+    
     if restart == "no":
         should_continue = False
         print("Goodbye")
